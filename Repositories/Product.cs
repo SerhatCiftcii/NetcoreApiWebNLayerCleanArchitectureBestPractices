2db51d1fb6a0af54fe7ab6cfe2172555b2d51a99
Serhat <s.ciftci561@gmail.com>
2025-05-16 20:28:59 +0300

    Add project files.

---
+++ Repositories/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repositories
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }= default!;//hala null değer alabilir ama default! ile null olamaz dedik
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
